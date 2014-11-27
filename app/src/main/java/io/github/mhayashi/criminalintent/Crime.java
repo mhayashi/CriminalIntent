ece2fedac51aeb1721bf221ab62045db5d56c10d
Masahiro Hayashi <hayashi.masahiro@gmail.com>
2014-11-28 01:47:14 +0900

    Add Crime

--- app/src/main/java/io/github/mhayashi/criminalintent/Crime.java
+++ app/src/main/java/io/github/mhayashi/criminalintent/Crime.java
@@ -1,7 +1,27 @@
 package io.github.mhayashi.criminalintent;
 
+import java.util.UUID;
+
 /**
  * Created by Masahiro Hayashi on 11/28/14.
  */
 public class Crime {
+    private UUID mID;
+    private String mTitle;
+
+    public Crime() {
+        mID = UUID.randomUUID();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
 }
