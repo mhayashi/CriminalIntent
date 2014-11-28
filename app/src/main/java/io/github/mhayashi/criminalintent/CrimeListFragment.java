c507ef65f16bc80a17c7729582e13309fc5c67e8
Masahiro Hayashi <hayashi.masahiro@gmail.com>
2014-11-29 02:07:42 +0900

    Load list items

--- app/src/main/java/io/github/mhayashi/criminalintent/CrimeListFragment.java
+++ app/src/main/java/io/github/mhayashi/criminalintent/CrimeListFragment.java
@@ -2,6 +2,10 @@ package io.github.mhayashi.criminalintent;
 
 import android.support.v4.app.ListFragment;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -9,13 +13,25 @@ import java.util.ArrayList;
  * Created by Masahiro Hayashi on 11/29/14.
  */
 public class CrimeListFragment extends ListFragment {
+    private static final String TAG = "CrimeListFragment";
     private ArrayList<Crime> mCrimes;
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Crime c = (Crime)(getListAdapter()).getItem(position);
+        Log.d(TAG, c.getTitle() + " was clicked");
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),
+                                                              android.R.layout.simple_list_item_1,
+                                                              mCrimes);
+        setListAdapter(adapter);
     }
 
 }
