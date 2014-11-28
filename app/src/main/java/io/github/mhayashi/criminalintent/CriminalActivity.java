64afc5ae0b6a14c86228839a036c5639a2423069
Masahiro Hayashi <hayashi.masahiro@gmail.com>
2014-11-28 18:34:11 +0900

    Initialize CrimeFragment

--- app/src/main/java/io/github/mhayashi/criminalintent/CriminalActivity.java
+++ app/src/main/java/io/github/mhayashi/criminalintent/CriminalActivity.java
@@ -1,8 +1,10 @@
 package io.github.mhayashi.criminalintent;
 
 import android.app.Activity;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,6 +15,14 @@ public class CriminalActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criminal);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
     }
 
 }
