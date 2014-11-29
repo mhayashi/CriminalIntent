b9c2f25e650ad62959c94177b5e32f567b3e7631
Masahiro Hayashi <hayashi.masahiro@gmail.com>
2014-11-29 22:23:37 +0900

    Add subtitle

--- app/src/main/java/io/github/mhayashi/criminalintent/CrimeListFragment.java
+++ app/src/main/java/io/github/mhayashi/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package io.github.mhayashi.criminalintent;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.support.v4.app.ListFragment;
 import android.os.Bundle;
@@ -62,6 +63,7 @@ public class CrimeListFragment extends ListFragment {
         inflater.inflate(R.menu.fragment_crime_list, menu);
     }
 
+    @TargetApi(11)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -71,6 +73,15 @@ public class CrimeListFragment extends ListFragment {
                 Intent i = new Intent(getActivity(), CrimePagerActivity.class);
                 i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getID());
                 startActivityForResult(i, 0);
+                return true;
+            case R.id.menu_item_show_subtitle:
+                if (getActivity().getActionBar().getSubtitle() == null) {
+                    getActivity().getActionBar().setSubtitle(R.string.subtitle);
+                    item.setTitle(R.string.hide_subtitle);
+                } else {
+                    getActivity().getActionBar().setSubtitle(null);
+                    item.setTitle(R.string.show_subtitle);
+                }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
